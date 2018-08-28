ecb37cb3f2a0477d4dc9a9a4da8adb4cd66ea4f2
Oscar <funjongsoo@gmail.com>
2018-08-28 12:29:30 +0900

    CounterCountainer 주석 추가

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -3,11 +3,13 @@ import * as actions from '../actions';
 import { connect } from 'react-redux';
 import { getRandomColor } from '../utils';
 
+// store의 state를 props로 매핑하여 넘김
 const mapStateToProps = (state) => ({
     number: state.numberData.number,
     color: state.colorData.color
 });
 
+// action을 dispatch하는 함수를 props로 연결
 const mapDispatchToProps = (dispatch) => ({
     onIncrement: () => dispatch(actions.increment()),
     onDecrement: () => dispatch(actions.decrement()),
@@ -16,6 +18,11 @@ const mapDispatchToProps = (dispatch) => ({
         dispatch(actions.setColor(color));
     }
 });
+
+// store가 가진 state를 props로 엮고
+// reducer에 action을 dispatch하는 함수를 props로 넘기고
+// 위 두가지가 적용된 nonFunction(Counter)가 반환됨
+// Counter를 param으로 갖는 func를 return
 
 const CounterContainer = connect(
     mapStateToProps,
