a6e83ef329c522ba33faffd7175027fa28fe7143
funjongsoo <funjongsoo@gmail.com>
2022-11-26 17:35:29 +0900

    react 9장 styled-component

---
+++ styling-react/src/App.js
@@ -0,0 +1,20 @@
+//import styles from './App.module.css';
+//import styles from './App.scss';
+//import classNames from 'classnames/bind';
+//import Button from './components/Button';
+import React from 'react';
+import StyledButton from './components/StyledButton';
+
+//const cx = classNames.bind(styles);
+
+//const isBlue = true;
+
+function App() {
+  return (
+    <div>
+      <StyledButton big>버튼</StyledButton>
+    </div>
+  );
+}
+
+export default App;
