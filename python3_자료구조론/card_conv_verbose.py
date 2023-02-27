abc088e5e81ee53ccbdaf9e517fdf19870330de7
funjongsoo <funjongsoo@gmail.com>
2023-02-27 22:04:45 +0900

    0227

---
+++ python3_자료구조론/card_conv_verbose.py
@@ -0,0 +1,40 @@
+# 10진수 정숫값을 입력받아 2~36진수로 변환하여 출력하기
+
+def card_conv(x: int, r: int) -> str:
+    """정숫값 x를 r진수로 변환한 뒤 그 수를 나타내는 문자열로 반환"""
+
+    d = '' # 변환 후의 문자열
+    dchar = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+    n = len(str(x))
+
+    print(f'{r:2} | {x:{n}d}')
+    while x > 0:
+        print('    +' + (n + 2) * '-')
+        d += dchar[x % r] # 해당하는 문자를 꺼내 결합
+        x //= r
+    
+    return d[::-1] # 역순으로 반환
+
+
+if __name__ == '__main__':
+    print('10진수를 n진수로 변환합니당.')
+
+    while True:
+        while True: # 음이 아닌 정수를 입력받음
+            no = int(input('변환할 값으로 음이 아닌 정수를 입력하세요. : '))
+            if no > 0:
+                break
+        
+        while True: # 2~36진수의 정숫값을 입력받음
+            cd = int(input('어떤 진수로 변환할까요? : '))
+            if 2 <= cd <= 36:
+                break
+        
+        print(f'{cd} 진수로는 {card_conv(no, cd)}입니다.')
+
+
+        retry = input("한번 더 변환할까요? Y or N : ")
+        if retry in {'N', 'n'}:
+            break
+        
