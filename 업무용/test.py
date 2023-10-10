314da73b218a0b2f9c94f103bcf09f9ece8e9484
funjongsoo <funjongsoo@gmail.com>
2023-10-10 17:35:45 +0900

    231010

--- 업무용/test.py
+++ 업무용/test.py
@@ -1,12 +1,20 @@
 import re
 
 
+# if __name__ == '__main__':
+#     excldPattern = re.compile("^.*60MIN.*$|.*Downlink Resource.*")
+
+#     msg = "5MIN/20230220/0620-0625-Downlink Resource Allocation per Slot and Table"
+
+#     if excldPattern.match(msg):
+#         print("true")
+#     else:
+#         print("false")
+
+
 if __name__ == '__main__':
-    excldPattern = re.compile("^.*60MIN.*$|.*Downlink Resource.*")
-
-    msg = "5MIN/20230220/0620-0625-Downlink Resource Allocation per Slot and Table"
-
-    if excldPattern.match(msg):
-        print("true")
+    element = 'RAN-EMS0054_3G'
+    if element.startswith('RAN-EMS') and element.endswith('_3G'):
+        print('yes')
     else:
-        print("false")
+        print('no')
