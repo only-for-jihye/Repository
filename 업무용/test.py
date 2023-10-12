d2f79dad4a547bc2dea1c25e2c23eb2fcab20de4
funjongsoo <funjongsoo@gmail.com>
2023-10-12 17:36:34 +0900

    1012

--- 업무용/test.py
+++ 업무용/test.py
@@ -13,8 +13,32 @@ import re
 
 
 if __name__ == '__main__':
-    element = 'RAN-EMS0054_3G'
-    if element.startswith('RAN-EMS') and element.endswith('_3G'):
-        print('yes')
+    # element = 'RAN-EMS0054'
+    # if element.startswith('RAN-EMS') and (element.endswith('_3G') or element.endswith('_5G')):
+    #     print('yes')
+    # else:
+    #     print('no')
+    # name = '1815-1820-ABC-AMF-22.A.0.csv'
+    # split = name.split('-')
+    # org = split[len(split)-1]
+    # print(org)
+
+    # pkgVer = '23.03.08'
+
+    # st = re.fullmatch('(\d{2}).(\d{2}).(\d{2})', pkgVer)
+
+    # if st:
+    #     print('yes')
+    # else:
+    #     print('no')
+    # print(st)
+    
+    name = 'ALARM.22'
+    arg = ' '
+    ver = name.split(arg)
+    print(ver)
+
+    if len(ver) > 1:
+        print(ver)
     else:
         print('no')
